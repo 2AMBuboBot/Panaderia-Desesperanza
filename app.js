ea888ca544756fb7f7445d55ebd898ccdf16b58d
2AMBuboBot <sifuenteshugo1309@gmail.com>
2025-11-02 00:07:32 -0600

    Sesiones

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Conexión a MySQL
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "n0m3l0",
-  database: "panaderia",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
