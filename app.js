9bdc8b329e9674ca842e8e91158ad8007f5cd7b7
2AMBuboBot <sifuenteshugo1309@gmail.com>
2025-11-06 23:45:33 -0600

    Hosteo 6.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: true,
+  origin: "http://localhost:3000",
   credentials: true
 }));
 app.use(express.json());
@@ -46,8 +46,8 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60, // 1 hora
     httpOnly: true,
-    secure: true,
-    sameSite: "none"
+    secure: false,
+    sameSite: "lax"
   }
 }));
 
